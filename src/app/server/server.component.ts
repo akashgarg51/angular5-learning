aa034ab391c6de9fce22d43c1b077248c5f1b97e
akashgarg <akashgarg51@gmail.com>
2017-12-03 13:32:52 +0530

    Ses 2-Lec 36, Attribute directives ngStyle, ngClass

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -3,13 +3,25 @@ import {selector} from 'rxjs/operator/publish';
 
 @Component({
   selector: 'app-server',
-  templateUrl: './server.component.html'
+  templateUrl: './server.component.html',
+  styles: [`
+    .online {
+      color: white;
+    }
+  `]
 })
 export class ServerComponent {
   serverId: number = 10;
   serverStatus: string = 'offline';
 
+  constructor() {
+    this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
+  }
   getServerStatus() {
     return this.serverStatus;
+  }
+
+  getColor() {
+    return this.serverStatus === 'online' ? 'green' : 'red';
   }
 }
