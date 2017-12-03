e763e9ba46522602601c7b8c4bdac7e4bf093f9c
akashgarg <akashgarg51@gmail.com>
2017-12-03 15:11:32 +0530

    Ses 2-Lec 38, Assignment and ngFor with index

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppComponent } from './app.component';
 import {FormsModule} from '@angular/forms';
 import {ServerComponent} from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
+import { Asgmt3DirectivesComponent } from './asgmt3-directives/asgmt3-directives.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
-    ServersComponent
+    ServersComponent,
+    Asgmt3DirectivesComponent
   ],
   imports: [
     BrowserModule,
